36c632f3e30ee91b2962452a57c83d6070b49dc0
Daniel Luan Tran <daniel.luan.tran@gmail.com>
2021-06-28 22:48:31 +0700

    Update

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -8,18 +8,18 @@ using System.Threading.Tasks;
 
 namespace InAndOut.Controllers
 {
-    public class ItemController : Controller
+    public class ExpenseController : Controller
     {
         private readonly ApplicationDbContext _db;
 
-        public ItemController(ApplicationDbContext db)
+        public ExpenseController(ApplicationDbContext db)
         {
             _db = db;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Item> objList = _db.Items;
+            IEnumerable<Expense> objList = _db.Expenses;
             return View(objList);
         }
 
@@ -32,9 +32,9 @@ namespace InAndOut.Controllers
         // POST-Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Item obj)
+        public IActionResult Create(Expense obj)
         {
-            _db.Items.Add(obj);
+            _db.Expenses.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Migrations/20210628150734_addExpense.Designer.cs
+++ Migrations/20210628150734_addExpense.Designer.cs
@@ -3,14 +3,16 @@ using InAndOut.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InAndOut.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210628150734_addExpense")]
+    partial class addExpense
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -28,7 +30,7 @@ namespace InAndOut.Migrations
                     b.Property<int>("Cost")
                         .HasColumnType("int");
 
-                    b.Property<string>("ExpenseName")
+                    b.Property<string>("ItemName")
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
